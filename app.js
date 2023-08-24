a4f104556ae92a5e720c80f2c69f465e79b5c70a
VirContreras <135673842+VirContreras@users.noreply.github.com>
2023-08-23 23:28:51 -0300

    primeros pasos CRUD

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+const bodyParser=require('body-parser');
+
+app.use(express.static('public'));
+
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3000,()=>{console.log("Server corriendo en el puerto 3000")})
